46eb7920e883e80b6a1aae8de933581983c2044f
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-12 15:04:28 +0100

    feat: teams exercise complete

--- Squadre/js/scripts.js
+++ Squadre/js/scripts.js
@@ -30,3 +30,23 @@ const teams = [
         fouls: 0
     }
 ]
+
+const getRandomNum = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+const teamsAndFouls = [];
+
+for (let i = 0; i < teams.length; i++) {
+    teams[i].points = getRandomNum(1, 10);
+    teams[i].fouls = getRandomNum(1, 5);
+    const curTeam = teams[i];
+
+    const teamData = {
+        name: curTeam.name,
+        fouls: curTeam.fouls
+    }
+
+    teamsAndFouls.push(teamData);
+}
+
+console.log(teamsAndFouls);
+
+
