227b49fec91d738ce4101747a3a0fd142acaf3e7
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-12 14:39:10 +0100

    feat: football teams array added

---
+++ Squadre/js/scripts.js
@@ -0,0 +1,32 @@
+const teams = [
+    {
+        name: "Stonewall Stormers",
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: "Dragon United",
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: "Hufflepuff",
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: "Slytherin",
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: "Gryffindor",
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: "Ravenclaw",
+        points: 0,
+        fouls: 0
+    }
+]
